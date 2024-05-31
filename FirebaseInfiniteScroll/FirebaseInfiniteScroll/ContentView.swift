765b160232c49963bf4b7f537d31e63cf1ff4763
sandlergiovanni <sandlergiovanni@gmail.com>
2024-05-31 13:40:31 -0300

    Adding correct project type.

---
+++ FirebaseInfiniteScroll/FirebaseInfiniteScroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseInfiniteScroll
+//
+//  Created by Sandler Maciel on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseInfiniteScroll/FirebaseInfiniteScroll/FirebaseInfiniteScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseInfiniteScrollApp.swift
+//  FirebaseInfiniteScroll
+//
+//  Created by Sandler Maciel on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseInfiniteScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
