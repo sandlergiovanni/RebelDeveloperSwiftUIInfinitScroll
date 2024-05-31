245e149d8129964595b3b9d709de23d2c2193d9e
sandlergiovanni <sandlergiovanni@gmail.com>
2024-05-31 13:30:10 -0300

    Creating a new project.

---
+++ FirebaseInfiniteScroll/FirebaseInfiniteScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseInfiniteScroll
+//
+//  Created by Sandler Maciel on 31/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
