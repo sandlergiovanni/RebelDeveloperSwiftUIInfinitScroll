3a0fa63897a37af6ba5638417840e3372deeb88a
sandlergiovanni <sandlergiovanni@gmail.com>
2024-06-01 07:56:15 -0300

    Adding firestore reference to manage user tasks.

--- FirebaseInfiniteScroll/FirebaseInfiniteScroll/Scenes/ContentView.swift
+++ FirebaseInfiniteScroll/FirebaseInfiniteScroll/Scenes/ContentView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
+import FirebaseFirestore
 
 struct ContentView: View {
+    @State private var limit = 1
+    
+    @FirestoreQuery(collectionPath: "userTasks", predicates:[.limit(to: 1)])
+    private var todoList: [UserTasks]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
